93268630eea9080ccd210bdd4945ffe8ce0ce3c8
mspainkiller <ms3painkiller@gmail.com>
2022-07-19 22:46:19 +0200

    Added task from section 4.3 step 3

--- test_product_page.py
+++ test_product_page.py
@@ -3,8 +3,18 @@ import time
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_about_adding_product_to_basket()
+    page.should_be_correct_basket_counter()
+
+
+def test_guest_can_add_product2_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
